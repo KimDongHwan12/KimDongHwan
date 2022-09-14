a55c60db487b22c03a31f6db43303b9d3eb2f454
KimDongHwan12 <112678987+KimDongHwan12@users.noreply.github.com>
2022-09-14 14:59:25 +0900

    Add files via upload

---
+++ hw1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int result1, result2, result3;
+	int num1, num2;
+
+	printf("input two integers: ");
+	scanf_s("%d %d", &num1, &num2);
+
+	result1 = num1 & num2;
+	printf("%d&%d=%d \n", num1, num2, result1);
+	
+	result2 = num1 | num2;
+	printf("%d|%d=%d \n", num1, num2, result2);
+	
+
+	result3 = num1 ^ num2;
+	printf("%d^%d=%d \n", num1, num2, result3);
+
+	return 0;
+}
+
